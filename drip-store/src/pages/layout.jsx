8c015025a42c0c4a0ce4f421491d9d46ed972146
Samilly Dias <samilly.s22s@gmail.com>
2024-12-16 19:14:17 +0000

    Atualizações no projeto: organização de páginas e dependências

--- drip-store/src/pages/layout.jsx
+++ drip-store/src/pages/layout.jsx
@@ -1,7 +1,7 @@
 import Header from "../Compoments/Header/Header"
 import Footer from "../Compoments/Footer/Footer"
-
-
+import { BrowserRouter } from "react-router-dom"
+import Rotas from "../Rotas/Rotas.jsx"
 const Layout = () => {
  return (
 
@@ -19,3 +19,4 @@ const Layout = () => {
 
 
     export default Layout
+    
